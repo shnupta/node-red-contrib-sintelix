2a0ac013b269a01398f1c36a9ce07f9fc171c404
Casey Williams <williamshoops96@gmail.com>
2019-03-19 15:20:44 +0000

    Added list projects node. Writing nodes to output all sorts of useful info. Especially for dashboard output.

--- nodes/sintelix-system-health.js
+++ nodes/sintelix-system-health.js
@@ -10,8 +10,11 @@ module.exports = function(RED) {
         this.on('input', function(msg) {
             // get the system health here
             this.sintelixConfig.post(`${credentials.host}/system/health`).then(function(response) {
-                msg.payload = response.body;
-                node.send(msg);
+                var mem = {};
+                var task = {};
+                mem.payload = 100 - parseInt(JSON.parse(response.body).memFree.slice(0, -2));
+                task.payload = JSON.parse(response.body).taskCount;
+                node.send([mem, task]);
             }).catch(function(err) {
                 node.error(err);
             });
